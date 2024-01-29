2da40970ecb26dafd156cf2bc89565fa1a7658d8
Janis Berneker <janis.berneker@typewise.app>
2024-01-29 11:53:02 +0100

    Further cleanups

--- abalytics/analysis.py
+++ abalytics/analysis.py
@@ -16,8 +16,8 @@ from .significance_tests import (
 
 
 class AnalysisResults:
-    def __init__(self, significant_results, info, sample_size, boolean_flag, levene_flag, gaussian_flag):
-        self.significant_results = significant_results
+    def __init__(self, significant_results=None, info=None, sample_size=None, boolean_flag=None, levene_flag=None, gaussian_flag=None):
+        self.significant_results = significant_results if significant_results is not None else []
         self.info = info
         self.sample_size = sample_size
         self.boolean_flag = boolean_flag
@@ -36,11 +36,13 @@ def get_results(df, variable_to_analyze, group_column, p_value_threshold=0.05):
     p_value_threshold (float, optional): The threshold for determining statistical significance. Defaults to 0.05.
 
     Returns:
-    tuple: A tuple containing the following elements:
-        - A list of results of the statistical significance tests.
-        - A boolean flag indicating if the variable is boolean.
-        - A boolean flag indicating if Levene's test for homogeneity of variances is significant.
-        - A boolean flag indicating if the data has a Gaussian distribution.
+    AnalysisResults: An instance of the AnalysisResults class containing the following attributes:
+        - significant_results: A list of results of the statistical significance tests.
+        - info: A string containing information about the data if no results were found.
+        - sample_size: The sample size of the data.
+        - boolean_flag: A boolean flag indicating if the variable is boolean.
+        - levene_flag: A boolean flag indicating if Levene's test for homogeneity of variances is significant.
+        - gaussian_flag: A boolean flag indicating if the data has a Gaussian distribution.
     """
     if p_value_threshold > 0.05:
         print(
@@ -62,7 +64,7 @@ def get_results(df, variable_to_analyze, group_column, p_value_threshold=0.05):
         # Perform chi-square test
         pvalue = get_chi_square_significance(df, group_column, variable_to_analyze)
         if pvalue < p_value_threshold:
-            significant_results = get_chi_square_posthoc_results(
+            results = get_chi_square_posthoc_results(
                 df, group_column, variable_to_analyze, p_value_threshold
             )
     elif is_numeric(df, variable_to_analyze):
@@ -72,7 +74,7 @@ def get_results(df, variable_to_analyze, group_column, p_value_threshold=0.05):
         ):
             pvalue = get_welch_anova_significance(df, group_column, variable_to_analyze)
             if pvalue < p_value_threshold:
-                significant_results = get_games_howell_posthoc_results(
+                results = get_games_howell_posthoc_results(
                     df, group_column, variable_to_analyze, p_value_threshold
                 )
         # Check if the data has Gaussian distribution, if not use non-parametric tests
@@ -83,7 +85,7 @@ def get_results(df, variable_to_analyze, group_column, p_value_threshold=0.05):
                 df, group_column, variable_to_analyze
             )
             if pvalue < p_value_threshold:
-                significant_results = get_tukeyhsd_posthoc_results(
+                results = get_tukeyhsd_posthoc_results(
                     df, group_column, variable_to_analyze, p_value_threshold
                 )
         else:
@@ -91,20 +93,21 @@ def get_results(df, variable_to_analyze, group_column, p_value_threshold=0.05):
                 df, group_column, variable_to_analyze
             )
             if pvalue < p_value_threshold:
-                significant_results = get_dunn_posthoc_results(
+                results = get_dunn_posthoc_results(
                     df, group_column, variable_to_analyze, p_value_threshold
                 )
     else:
         print("Variable has to be boolean or numeric")
 
-    # If no significant results were found, return a message
-    if not 'significant_results' in locals() or len(significant_results.significant_results) == 0:
+    # If no results were found, return a message
+    if not 'results' in locals() or len(results.significant_results) == 0:
         if pvalue is not None:
             info = f"not significant (p={pvalue:.3f})"
-    if 'significant_results' not in locals():
-        significant_results = {
-            "significant_results": []
-        }
+    # If results were found, return them
+    if 'results' in locals():
+        significant_results = results.significant_results
+    else:
+        significant_results = []
 
     return AnalysisResults(
         significant_results, info, sample_size, boolean_flag, levene_flag, gaussian_flag
@@ -160,7 +163,7 @@ def get_results_pretty_text(
     analysis_results = get_results(
         df, variable_to_analyze, group_column, p_value_threshold
     )
-    results = analysis_results.significant_results
+    significant_results = analysis_results.significant_results
     info = analysis_results.info
     sample_size = analysis_results.sample_size
     boolean_flag = analysis_results.boolean_flag
@@ -185,8 +188,8 @@ def get_results_pretty_text(
     gaussian_output = "X" if gaussian_flag else ""
 
     output_results = []
-    if hasattr(results, 'significant_results') and results.significant_results:
-        for i, result in enumerate(results.significant_results):
+    if len(significant_results) > 0:
+        for i, result in enumerate(significant_results):
             if i == 0:
                 output_results.append(
                     f"{identifier_string}{sample_size:>10}    {boolean_output:<7}    {levene_output:<6}    {gaussian_output:<8}    {result.result_pretty}"

611b0a423b6c2e1567e669c78aedcb59c78eebe3
Janis Berneker <janis.berneker@typewise.app>
2024-02-25 02:37:34 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='abalytics',
-    version='2.1.2',
+    version='3.0.1',
     author='Janis Berneker',
     packages=find_packages(),
     license='LICENSE',
